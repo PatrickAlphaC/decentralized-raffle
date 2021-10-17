b5b4b3b1d833a1d5deb56d463489c1d90aa17d15
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2021-10-16 21:32:55 -0400

    init commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+import "hardhat/console.sol";
+
+contract Raffle is
+    Ownable,
+    VRFConsumerBase,
+    ReentrancyGuard,
+    KeeperCompatibleInterface
+{
+    bytes32 public s_keyHash;
+    uint256 public s_chainlinkFee;
+    uint256 public s_entranceFee;
+    uint256 public s_lastTimeStamp;
+    uint256 public s_interval;
+    address public s_recentWinner;
+    address payable[] public s_players;
+
+    event requestedRaffleWinner(bytes32 indexed requestId);
+    event enteredRaffle(address indexed player);
+    event winnerPicked(address indexed player);
+
+    constructor(
+        address _vrfCoordinator,
+        address _linkToken,
+        bytes32 _keyHash,
+        uint256 _chainlinkFee,
+        uint256 _entranceFee,
+        uint256 _interval
+    ) VRFConsumerBase(_vrfCoordinator, _linkToken) {
+        s_lastTimeStamp = block.timestamp;
+        s_keyHash = _keyHash;
+        s_chainlinkFee = _chainlinkFee;
+        s_entranceFee = _entranceFee;
+        s_interval = _interval;
+    }
+
+    function enterRaffle() public payable {
+        require(
+            msg.value >= s_entranceFee,
+            "Not enough value sent to enter raffle"
+        );
+        s_players.push(payable(msg.sender));
+        emit enteredRaffle(msg.sender);
+    }
+
+    // receive() external payable {}
+
+    /**
+     * @dev This is the function that the Chainlink Keeper nodes call
+     * they look for `upkeepNeeded` to return True
+     * the following should be true for this to return true:
+     * 1. The time interval has passed between raffle runs
+     * 2. The contract has LINK
+     * 3. The contract has ETH
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool hasLink = LINK.balanceOf(address(this)) >= s_chainlinkFee;
+        upkeepNeeded = (((block.timestamp - s_lastTimeStamp) > s_interval) &&
+            hasLink &&
+            (address(this).balance >= 0));
+    }
+
+    /**
+     * @dev Once `checkUpkeep` is returning `true`, this function is called
+     * and it kicks off a Chainlink VRF call to get a random winner
+     */
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        require(
+            LINK.balanceOf(address(this)) >= s_chainlinkFee,
+            "Not enough LINK"
+        );
+        require(address(this).balance >= 0, "Not enough ETH");
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        require(upkeepNeeded, "Upkeep not needed");
+        s_lastTimeStamp = block.timestamp;
+        bytes32 requestId = requestRandomness(s_keyHash, s_chainlinkFee);
+        emit requestedRaffleWinner(requestId);
+    }
+
+    /**
+     * @dev This is the function that Chainlink VRF node calls to send the money to the random winner
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        uint256 index = randomness % s_players.length;
+        address payable recentWinner = s_players[index];
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        s_recentWinner = recentWinner;
+        emit winnerPicked(recentWinner);
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,64 @@
+import * as dotenv from "dotenv"
+
+import { HardhatUserConfig, task } from "hardhat/config"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-waffle"
+import "@typechain/hardhat"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
+import "hardhat-deploy"
+import "@appliedblockchain/chainlink-plugins-fund-link"
+import "@nomiclabs/hardhat-ethers"
+
+dotenv.config()
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.6"
+      },
+      {
+        version: "0.4.24"
+      },
+      {
+        version: "0.8.7"
+      },
+    ]
+  },
+  networks: {
+    kovan: {
+      url: process.env.KOVAN_RPC_RPC || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+}
+
+export default config
